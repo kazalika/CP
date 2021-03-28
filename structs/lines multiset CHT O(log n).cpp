3aa13b973ee992263d78f7fc64ba341ad71adc6a
kazalika <37236031+kazalika@users.noreply.github.com>
2021-03-28 10:36:10 +0400

    Update lines multiset CHT O(log n).cpp

--- structs/lines multiset CHT O(log n).cpp
+++ structs/lines multiset CHT O(log n).cpp
@@ -2,43 +2,37 @@
 using namespace std;
 
 /******************************************************
-
      Convex hull trick by simonlindholm
      source : https://github.com/kth-competitive-programming/kactl/blob/master/content/data-structures/LineContainer.h
      O(log n) for query
      doesn't need any trees except of multiset
      +add line (k, m)
      +get maximum y-coordinate for x in current lines
-
 ******************************************************/
-
 
 typedef long long ll;
 
 struct Line {
-  mutable ll k, m, p;
-  bool operator<(const Line& o) const { return k < o.k; }
-  bool operator<(ll x) const { return p < x; }
+  mutable ll k, m, r;
+  bool operator< (const Line &o) const { return k < o.k; }
+  bool operator< (ll x) const { return r < x; }
 };
 
-struct LineContainer : multiset<Line, less<>> {
-  // (for doubles, use inf = 1/.0, div(a,b) = a/b)
+// если double а не ll заменяем div на просто a / b
+struct LineConteiner : multiset<Line, less<>> {
   static const ll inf = LLONG_MAX;
-  ll div(ll a, ll b) { // floored division
-    return a / b - ((a ^ b) < 0 && a % b);
-  }
-  bool isect(iterator x, iterator y) {
-    if (y == end()) return x->p = inf, 0;
-    if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
-    else x->p = div(y->m - x->m, x->k - y->k);
-    return x->p >= y->p;
+  ll div(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); }       // деление с округлением вниз в том числе для отрицательных чисел (самое по себе деление в c++ работает криво)
+  bool isect(iterator cur, iterator nxt) {                            // нужно ли удалить nxt?
+    if (nxt == end()) return cur->r = inf, 0;                         // если следующей прямой нет r = inf и return false;
+    if (cur->k == nxt->k) cur->r = cur->m > nxt->m ? inf : -inf;      // если коэф k равны: если cur выше nxt, то r = inf иначе r = -inf
+    else cur->r = div(nxt->m - cur->m, cur->k - nxt->k);              // если коэф k неравны: находим точку пересечения cur и nxt: x = (m2 - m1) / (k1 - k2)
+    return cur->r >= nxt->r;                                          // если cur до nxt->r лучше nxt, то возвращаем true
   }
   void add(ll k, ll m) {
-    auto z = insert({k, m, 0}), y = z++, x = y;
-    while (isect(y, z)) z = erase(z);
-    if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
-    while ((y = x) != begin() && (--x)->p >= y->p)
-      isect(x, erase(y));
+    auto z = insert({k, m, 0}), y = z++, x = y;                        // пихаем в multiset нашу прямую x и y указывают на нашу прямую, z - на ту, что правее
+    while (isect(y, z)) z = erase(z);                                  // пока справа прямая хуже нашей isect(y, z), удаляем ее z = erase(z)
+    if (x != begin() && isect(--x, y)) isect(x, y = erase(y));         // случай если прямая левее (--x) лучше нашей, тогда просто удаляем нашу y = erase(y)
+    while ((y = x) != begin() && (--x)->r >= y->r) isect(x, erase(y)); // видимо это на всякий случай...
   }
   ll query(ll x) {
     assert(!empty());
