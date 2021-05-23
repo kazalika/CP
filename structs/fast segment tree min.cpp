184203ca500f28df09780b674cb8205ee276a998
kazalika <37236031+kazalika@users.noreply.github.com>
2021-05-23 11:07:33 +0400

    Update fast segment tree min.cpp

--- structs/fast segment tree min.cpp
+++ structs/fast segment tree min.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+
 /**************************************************
-      Segment Tree on minimum by Kazalika
+      Segment Tree on maximum by Kazalika
                  O(log) query
+             3.684s (n = 2e7, int)
 **************************************************/
 
 template <typename T>
@@ -12,33 +15,61 @@ struct segment_tree {
   T inf;
   vector<T> t;
   segment_tree(vector<T> &v) {
-    inf = INT_MAX;
-    if (inf + 1 > 0)
+    inf = INT_MAX + 1;
+    if (inf > 0)
       inf = LLONG_MAX;
+    else
+      inf--;
     size = 1;
     while (size < v.size())
       size <<= 1;
-    t.resize(size << 1);
-    for (int i = 0; i < size; ++i)
+    t.resize(size << 1, -inf);
+    for (int i = 0; i < v.size(); ++i)
       t[size + i] = v[i];
     for (int i = size - 1; i > 0; --i)
-      t[i] = min(t[i << 1], t[i << 1 | 1]);
+      t[i] = max(t[i << 1], t[i << 1 | 1]);
   }
   void upd(int i, T vl) {
     i += size - 1;
     t[i] = vl;
     while (i >>= 1)
-      t[i] = min(t[i << 1], t[t << 1 | 1]);
+      t[i] = max(t[i << 1], t[t << 1 | 1]);
   }
   T get(int l, int r) {
     l--;
-    T ret = inf;
+    T ret = -inf;
     for (int f = l + size, s = r + size; f < s; f >>= 1, s >>= 1) {
       if (f & 1)
-        ret = min(ret, t[f]), f++;
+        ret = max(ret, t[f]), f++;
       if (s & 1)
-        s--, ret = min(ret, t[s]);
+        s--, ret = max(ret, t[s]);
     }
     return ret;
   }
 };
+
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  ll b, c, d, x, ans = 0, res = 0;
+  cin >> a[0] >> b >> c >> d >> x;
+  for (int i = 1; i < n; ++i) {
+    b = (b * c + d) % (1LL << 31);
+    a[i] = (a[i - 1] + 1 - 2 * ((b % 239179) & 1));
+  }
+  segment_tree<int> sgt(a);
+
+  for (int i = 0; i < n; ++i) {
+    ll l = (x + ans % n + n) % n;
+    ll r = (l + i) % n;
+    x = (x * 1103515245 + 12345) % (1LL << 31);
+    ans = sgt.get(min(l, r) + 1, max(l, r) + 1);
+    res += ans;
+  }
+  cout << res;
+}
